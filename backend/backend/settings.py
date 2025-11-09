53e8b62079fc84132d751aa585d751c477038cc9
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-11-09 09:54:27 +0200

    feat(database): connect project to MySQL instead of SQLite

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', default='3306'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
